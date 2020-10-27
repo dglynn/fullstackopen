7d9a102c712be6dba714dd84f170f0fae75a5e2b
Darren Glynn <darren.glynn@gmail.com>
2020-10-27 08:53:18 +0200

    update first react app

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,34 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/dglynn">dglynn</a>
-    </div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
-  )
-}
+    <div>
+      <Display counter={counter} />
+      <Button
+        handleClick={increaseByOne}
+        text="plus"
+      />
+      <Button
+        handleClick={setToZero}
+        text="zero"
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text="minus"
+      />
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
