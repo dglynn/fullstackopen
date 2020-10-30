d9cb11545f570c0ade69ae18b402b06ccdfe10d9
Darren Glynn <darren.glynn@relexsolutions.com>
2020-10-30 09:01:18 +0200

    component state and event handlers

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,56 +1,52 @@
 import React, { useState } from "react";
 import ReactDOM from 'react-dom'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
 
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+const Display = props => <div>{props.value}</div>
 
-// const Button = ({ onClick, text }) => (
-//   <button onClick={onClick}>
-//     {text}
-//   </button>
-// )
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Button = (props) => {
-  console.log(props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+// const Button = (props) => {
+//   console.log(props)
+//   const { onClick, text } = props
+//   return (
+//     <button onClick={onClick}>
+//       {text}
+//     </button>
+//   )
+// }
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)  }
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    setValue(newValue)
   }
+
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
