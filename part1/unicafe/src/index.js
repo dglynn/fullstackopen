c9b20925653ea1804ab62da92b5ae2016e56c586
Darren Glynn <darren.glynn@relexsolutions.com>
2020-11-01 08:44:26 +0200

    Exercise 1.6 Unicafe

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({header}) => {
+  //console.log(header)
+  return (
+    <h1>
+      {header}
+    </h1>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.addClick}>
+    {props.text}
+  </button>
+)
+
+const Stats = ({stats}) => {
+  //console.log(stats)
+  return (
+    <h1>
+      {stats}
+    </h1>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <p>
+      good {props.good}
+      <br />
+      neutral {props.neutral}
+      <br />
+      bad {props.bad}
+    </p>
+  )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const header = "give feedback"
+  const stats = "statistics"
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header header={header}/>
+      <Button addClick={() => setGood(good + 1)} text="good" />
+      <Button addClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button addClick={() => setBad(bad + 1)} text="bad" />
+      <Stats stats={stats} />
+      <Total good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
